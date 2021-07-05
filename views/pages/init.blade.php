78c538044b25f5d593f44096349f69d109337327
abugraokkali <abugraokkali@outlook.com>
2021-07-05 11:58:49 +0300

    ui changes and cluster members list

--- views/pages/init.blade.php
+++ views/pages/init.blade.php
@@ -1,9 +1,52 @@
 <div id="infoDiv" style="visibility:none;"></div>
 <div id="errorDiv" style="visibility:none;"></div>
-<button class="btn btn-info mb-2" id="btn" style="float:left;margin-left:10px;visibility:none;"></button>
+<pre id="patroniInfo">   </pre>
+<button class="btn btn-info mb-2" id="btn" onclick="showInitModal()" style="float:left;margin-left:10px;visibility:none;"></button>
 
+@component('modal-component',[
+        "id" => "initModal",
+        "title" => "Initilization",
+    ])
+
+    <form>
+        <div class="form-group">
+            <label for="patroniIpAdress">{{__('Patroni IP')}}</label>
+            <input class="form-control" id="patroniIpAdress" aria-describedby="patroniIpAdressHelp" placeholder="{{__('Patroni IP')}}">
+            <small id="patroniIpAdressHelp" class="form-text text-muted">{{__('Patroni Sunucunuzun IP adresini giriniz (192.168.1.10)')}}.</small>
+        </div>
+        <div class="form-group">
+            <label for="etcdIpAdress">{{__('ETCD IP')}}</label>
+            <input class="form-control" id="etcdIpAdress" aria-describedby="etcdIpAdressHelp" placeholder="{{__('ETCD IP')}}">
+            <small id="etcdIpAdressHelp" class="form-text text-muted">{{__('ETCD sunucunuzun IP adresini giriniz.')}}</small>
+        </div>
+        <div class="form-group">
+            <label for="patroniName">{{__('Patroni Adı')}}</label>
+            <input class="form-control" id="patroniName" aria-describedby="patroniNameHelp" placeholder="{{__('Patroni Adı')}}">
+            <small id="patroniNameHelp" class="form-text text-muted">{{__('Patroni adını giriniz.')}}</small>
+        </div>
+        <div class="form-group">
+            <label for="patroniPassword">{{__('Parola')}}</label>
+            <input type="password" class="form-control" id="patroniPassword" placeholder="{{__('Parola')}}">
+            <small id="patroniPasswordHelp" class="form-text text-muted">{{__('Patroni parolasını giriniz.')}}</small>
+        </div>
+        <div class="form-group">
+            <label for="scopeName">{{__('Scope adı')}}</label>
+            <input class="form-control" id="scopeName" aria-describedby="scopeNameHelp" placeholder="{{__('Scope Adı')}}">
+            <small id="scopeNameHelp" class="form-text text-muted">{{__('Scope adını giriniz. (Opsiyonel)')}}</small>
+        </div>
+    </form>
+
+    <button class="btn btn-primary" onclick="initialize()" style="float:right;">{{__('initialize ')}}</button>
+    
+@endcomponent
 
 <script>
+
+    function showInitModal(){
+      $('#initModal').modal("show");
+    }
+
+    var flag = false;
     function isYmlExists(){
         var form = new FormData();
         request(API('is_yml_exists'), form, function(response) {
@@ -16,11 +59,12 @@
                         '{{__("Sunucuda patroni.yml dosyası tespit edildi ! Reinitialize etmek isterseniz butonu kullanabilirsiniz.")}}'+
                     '</div>'+
                 '</div>');
-
+            
             let button = document.getElementById("btn");
-            button.onclick = function() {reinit()};
             button.innerText = '{{__("Reinitalize")}}';
             button.style.visibility = "visible";
+            flag = true;
+            info();
             
         }, function(response) {
             $('#errorDiv').html(
@@ -33,15 +77,50 @@
                     '</div>');
             document.getElementById("addCluster_li").style.display = "none";
             let button = document.getElementById("btn");
-            button.onclick = function() {init()};
             button.innerText = '{{__("Initalize")}}';
             button.style.visibility = "visible";
+            flag = false;
         });
         
     }
+    function initialize(){
+        if(flag){
+            reinit();
+        }
+        else{
+            init();
+        }
+    }
 
+    function info(){
+        var form = new FormData();
+
+        request(API('info'), form, function(response) {
+            message = JSON.parse(response)["message"];
+            $("#patroniInfo").text(message);
+
+        }, function(response) {
+            let error = JSON.parse(response);
+            showSwal(error.message,'error',2000);
+
+        });
+    }
     function init(){
         var form = new FormData();
+
+        patroniIpAdress = document.getElementById("patroniIpAdress").value;
+        etcdIpAdress = document.getElementById("etcdIpAdress").value;
+        patroniName = document.getElementById("patroniName").value;
+        patroniPassword = document.getElementById("patroniPassword").value;
+        scopeName = document.getElementById("scopeName").value;
+
+        form.append("patroniIpAdress",patroniIpAdress);
+        form.append("etcdIpAdress",etcdIpAdress);
+        form.append("patroniName",patroniName);
+        form.append("patroniPassword",patroniPassword);
+        form.append("scopeName",scopeName);
+
+
         request(API('init'), form, function(response) {
             message = JSON.parse(response)["message"];
             successModal(message);
@@ -53,6 +132,19 @@
 
     function reinit(){
         var form = new FormData();
+
+        patroniIpAdress = document.getElementById("patroniIpAdress").value;
+        etcdIpAdress = document.getElementById("etcdIpAdress").value;
+        patroniName = document.getElementById("patroniName").value;
+        patroniPassword = document.getElementById("patroniPassword").value;
+        scopeName = document.getElementById("scopeName").value;
+
+        form.append("patroniIpAdress",patroniIpAdress);
+        form.append("etcdIpAdress",etcdIpAdress);
+        form.append("patroniName",patroniName);
+        form.append("patroniPassword",patroniPassword);
+        form.append("scopeName",scopeName);
+
         request(API('reinit'), form, function(response) {
             message = JSON.parse(response)["message"];
             successModal(message);
