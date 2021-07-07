ef9ef5caa2d9c2b1b5a9f8453ab29fac754a6801
abugraokkali <abugraokkali@outlook.com>
2021-07-07 16:04:18 +0300

    nodes of cluster backend

--- app/Controllers/PatroniController.php
+++ app/Controllers/PatroniController.php
@@ -22,7 +22,7 @@ class PatroniController
 
 		$init = "/etc/gopatroniyml init --etcd ". $etcd ." -i " . $patroniIp ." -n ". $patroniName ." --password ". $password ." --path /tmp/patroni.yml";
 		$initScope = "/etc/gopatroniyml init --etcd ". $etcd ." -i ". $patroniIp ." -n ". $patroniName ." --password ". $password." -s ". $scope ." --path /tmp/patroni.yml";
-		
+		shell_exec("touch /tmp/patroni.yml");
 		if($scope != ""){
 			shell_exec($initScope." > /tmp/output.txt");
 			shell_exec("echo ".$initScope." > /tmp/commandLog.txt");
@@ -60,7 +60,8 @@ class PatroniController
 
 		$init = "/etc/gopatroniyml reinit --etcd ". $etcd ." -i " . $patroniIp ." -n ". $patroniName ." --password ". $password ." --path /tmp/patroni.yml";
 		$initScope = "/etc/gopatroniyml reinit --etcd ". $etcd ." -i ". $patroniIp ." -n ". $patroniName ." --password ". $password." -s ". $scope ." --path /tmp/patroni.yml";
-		
+		shell_exec("touch /tmp/patroni.yml");
+	
 		if($scope != ""){
 			shell_exec($initScope." > /tmp/output.txt");
 			shell_exec("echo ".$initScope." > /tmp/commandLog.txt");
@@ -95,13 +96,57 @@ class PatroniController
 		$ip = request('ip');
 		$add = "/etc/gopatroniyml --appPatNode " . $ip. " --path /tmp/patroni.yml";
 		shell_exec($add);
-		
+
 		$remote_path = '/tmp/patroni.yml';
 		putFile('/tmp/patroni.yml', $remote_path);
 		runCommand(sudo().'mv /tmp/patroni.yml /etc/');
         $this->restart();
 
 		return respond("New node successfully added.",200);
+	}
+
+	function clusterInfo(){
+
+
+		$output = runCommand(sudo().'curl -s http://192.168.1.80:8008/cluster | jq .');
+		$decodedText = html_entity_decode($output);
+		$arr = json_decode($decodedText, true);
+		$tableData = [];
+		for ($i = 0; $i < count($arr["members"]); $i++) {
+			$tableData[] = [
+				"name" => $arr["members"][$i]["name"],
+				"role" => $arr["members"][$i]["role"],
+				"state" => $arr["members"][$i]["state"],
+				"api_url" => $arr["members"][$i]["api_url"],
+				"host" => $arr["members"][$i]["host"],
+				"port" => $arr["members"][$i]["port"],
+				"timeline" => $arr["members"][$i]["timeline"],
+		
+			];
+
+		}
+		//return respond($data['name'],200);
+
+		return view('table', [
+            "value" => $tableData,
+            "title" => ["Name","Role","State","*hidden*","Host","Port","Timeline"],
+            "display" => ["name","role","state","api_url:api_url","host","port","timeline"],
+			"onclick" => "getDetails"
+        ]);
+	}
+
+	function getDetails(){
+        $api_url = request("api_url");
+
+		$output = runCommand(sudo().'curl -s '.$api_url.' | jq .');
+		$decodedText = html_entity_decode($output);
+		$arr = json_decode($decodedText, true);
+		$str = "state : ".$arr["state"];
+		$str .= "\npostmaster_start_time : ".$arr["postmaster_start_time"];
+		$str .= "\nrole : ".$arr["role"];
+
+		return respond($str,200);
+
 	}
 
     function isYmlExists(){
--- routes.php
+++ routes.php
@@ -7,5 +7,13 @@ return [
     "set_hostname" => "HostnameController@set",
 
     "is_yml_exists" => "PatroniController@isYmlExists",
-    "init" => "PatroniController@init"
+    "init" => "PatroniController@init",
+    "reinit" => "PatroniController@reinit",
+    "hba" => "TableController@hbaTable",
+    "add" => "PatroniController@add",
+    "info" => "PatroniController@info",
+    "cluster_info" => "PatroniController@clusterInfo",
+    "get_details" => "PatroniController@getDetails"
+
+
 ];
